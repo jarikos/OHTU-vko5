21057b6c0b1b4eecf2da88cd705e27f61048b9e0
jarikos <jari.m.koskinen@helsinki.fi>
2012-04-22 22:30:02 +0300

    new file:   laskari5/IntJoukkoSovellus/src/test/java/IntJoukkoTest.java

---
+++ laskari5/IntJoukkoSovellus/src/test/java/IntJoukkoTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+
+/**
+ *
+ * @author JKosk2
+ */
+public class IntJoukkoTest {
+    
+    public IntJoukkoTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+}
